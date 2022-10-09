6668174a24e1e046d81098ffd3ce799d392b146a
Easin Webpro <easinislam299@gmail.com>
2022-10-09 23:49:33 +0600

    . change server.js and vercel.json file

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./src/db.json');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const jsonRouter = jsonServer.router('./src/db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-
-const port = process.env.PORT || 5050
-
+server.use(middlewares);
+server.use(jsonRouter);
 server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+  console.log('JSON Server is running');
+});
